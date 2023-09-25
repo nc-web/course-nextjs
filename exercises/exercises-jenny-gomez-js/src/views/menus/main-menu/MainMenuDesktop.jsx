df84f7949de49638e6116936d017945271eaf969
Jenny Gomez <jenny16parr@gmail.com>
2023-09-25 12:18:41 -0500

    New: enrutamiento y css en next13

---
+++ exercises/exercises-jenny-gomez-js/src/views/menus/main-menu/MainMenuDesktop.jsx
@@ -0,0 +1,36 @@
+
+import Link from "next/link"
+
+// Styles
+import styles from './main-menu-desktop.module.css'
+
+export default function MainMenuDesktop() {
+  return(
+    <>
+      <menu className={styles.main}>
+        <div className={styles.main__company}>
+           Company
+        </div>
+
+        <div className={styles.main__nav}>
+            <Link className={styles.main__nav_link} href="/">
+                <div className={styles.main__nav_items}>
+                    Home
+                </div>
+                
+            </Link>
+
+            <Link href="/login" className={styles.main__nav_link}>
+                <div className={styles.main__nav_items}>
+                    Login
+                </div>
+                </Link>
+        </div>
+
+        <div className={styles.main__login}>
+            Login
+        </div>
+      </menu>
+    </>
+  );
+}
