e36356db210c79edd48e747c396846cdc2e81f20
Andres Giraldo <andresganc@gmail.com>
2023-09-24 20:38:37 -0500

    Restructuring Course NC: Deploying, Upgrading, Releases

--- course-nc/releases/next-12-js/pages/01-basic/routing-static/index.jsx
+++ course-nc/releases/next-12-js/pages/01-basic/routing-static/index.jsx
@@ -7,7 +7,7 @@ import H2 from '@components/h2'
 import Button from '@components/button'
 
 // NC Components Advanced
-import Menu from '../../01-basic/routing-static/menu'
+import Menu from './menu'
 
 
 const navigation = () => {
