90f3263fc17444e1c19ab4487aba0ed1669b7079
Harrinson Herrera <harrinsonherrera13@gmail.com>
2023-09-25 12:14:00 -0500

    New: se creo menu y se hicieron pruebas de enrutamiento

--- exercises/exercises-harry-herrera-js/src/app/layout.js
+++ exercises/exercises-harry-herrera-js/src/app/layout.js
@@ -1,5 +1,9 @@
-import './globals.css'
+
+import './globals-styles.css'
 import { Inter } from 'next/font/google'
+
+// Menus
+import MainMenuDesktop from '@views/menus/main-menu/main-menu-desktop/MainMenuDesktop'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -11,7 +15,13 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+
+      <MainMenuDesktop></MainMenuDesktop>
+      
+      {children}
+      
+      </body>
     </html>
   )
 }
---
+++ exercises/exercises-harry-herrera-js/src/views/menus/main-menu/main-menu-desktop/MainMenuDesktop.jsx
@@ -0,0 +1,24 @@
+
+import Link from "next/link";
+
+// Styles
+import styles from './main-menu-desktop.module.css'
+
+export default function MainMenuDesktop() {
+  return (
+    <>
+      <menu className={styles.menu}>
+        <div className={styles.menu__logo}>
+          <p>Logo</p>
+        </div>
+        <div className={styles.menu__nav}>
+          <nav className={styles.menu__nav_container}>
+            <Link href='/' className={styles.menu__nav_container_link}><p>Home</p></Link>
+            <Link href='/login' className={styles.menu__nav_container_link}><p>Login</p></Link>
+            <Link href='/shop' className={styles.menu__nav_container_link}><p>Shop</p></Link>
+          </nav>
+        </div>
+      </menu>
+    </>
+  )
+}
