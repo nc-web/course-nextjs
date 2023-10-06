ea37cdd093c011377a0164eea7d737e0c5fd8beb
Andres Giraldo <andresganc@gmail.com>
2023-10-06 10:34:20 -0500

    New: Course NC: Routing: Examples and Exercises

---
+++ course-nc/03-routing/01-defining-routes/defining-routes.md
@@ -0,0 +1,6 @@
+
+# Defining Routes
+
+    - This page will guide you through how to define and organize routes in your Next.js application.
+
+    
---
+++ course-nc/03-routing/examples/basic-routing-js/src/app/page.jsx
@@ -0,0 +1,10 @@
+import Image from 'next/image'
+import styles from './page.module.css'
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <h4>Home Page</h4>
+    </main>
+  )
+}
