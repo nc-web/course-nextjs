d905ca97aff7b151c0cdfb8984314325c82c7e33
Andres Giraldo <andresganc@gmail.com>
2023-10-06 11:41:46 -0500

    Mod: Course NC: Routing: Basic Routing

---
+++ course-nc/03-routing/examples/basic-routing-js/src/app/login/page.jsx
@@ -0,0 +1,8 @@
+
+export default function Login() {
+    return(
+        <>
+            <h4>Login Page</h4>
+        </>
+    ) 
+}
---
+++ course-nc/03-routing/examples/basic-routing-js/src/app/not-found.jsx
@@ -0,0 +1,8 @@
+
+export default function NoFound() {
+    return(
+        <>
+            <h4>No Found Page</h4>
+        </>
+    )
+}
