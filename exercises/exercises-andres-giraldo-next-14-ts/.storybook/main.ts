0dc7d5442a49ec51d574906d2ae5e514ea0cd115
Andres Giraldo <andresganc@gmail.com>
2023-11-21 00:07:58 -0500

    Mod: Exercises: Exercises Andres Next 14 TS Coverage

--- exercises/exercises-andres-giraldo-next-14-ts/.storybook/main.ts
+++ exercises/exercises-andres-giraldo-next-14-ts/.storybook/main.ts
@@ -1,18 +1,16 @@
 
-// import type { AddonOptionsBabel } from '@storybook/addon-coverage';
-import type { AddonOptionsVite } from '@storybook/addon-coverage';
 import type { StorybookConfig } from "@storybook/nextjs";
 
 
 const config: StorybookConfig = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)", "../src/**/**/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-onboarding",
     "@storybook/addon-interactions",
     '@storybook/addon-a11y',
-    '@storybook/addon-coverage',
+    '@storybook/addon-coverage'
   ],
   framework: {
     name: "@storybook/nextjs",
